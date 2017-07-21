09548e38bfbfb791933cc06b085fbca5a7b547bd
Jeff Zivkovic <zivkovic.jeff@gmail.com>
2017-07-21 17:41:10 +0000

    Try with landing page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+  
+  def help
+  end
+
+  def contact
+  end
+  
+  def partner
+  end
+  
+  def about
+  end
+  
+  def materials
+  end
+end
