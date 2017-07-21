49d2f469a6e813ee4a6e094da4f4eccc31356452
Jeff Zivkovic <zivkovic.jeff@gmail.com>
2017-07-21 20:58:14 +0000

    Holy Moly This is a Process!

--- db/migrate/20170721202648_create_objective_users.rb
+++ db/migrate/20170721202648_create_objective_users.rb
@@ -1,8 +1,8 @@
 class CreateObjectiveUsers < ActiveRecord::Migration[5.0]
   def change
     create_join_table :objectives, :users do |t|
-      t.references  :objective_id, foreign_key: true
-      t.references  :user_id, foreign_key: true
+      t.references  :objectives, foreign_key: true
+      t.references  :users, foreign_key: true
       t.integer :score
       t.integer :unlocked
       t.timestamps
