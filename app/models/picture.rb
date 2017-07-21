8d25d456bb339d0be3ea9781a76e78ae5047d7c5
Jeff Zivkovic <zivkovic.jeff@gmail.com>
2017-07-21 15:53:29 +0000

    First commit for non-tutorial version

---
+++ app/models/picture.rb
@@ -0,0 +1,5 @@
+class Picture < ActiveRecord::Base
+    validates_presence_of :name
+    
+    mount_uploader :image, ImageUploader
+end
---
+++ db/migrate/20170721143919_create_pictures.rb
@@ -0,0 +1,12 @@
+class CreatePictures < ActiveRecord::Migration
+  def change
+    create_table :pictures do |t|
+      t.string :name
+      t.references :label, foreign_key: true
+      t.references :questions, foreign_key: true
+      t.string :image
+      
+      t.timestamps null: false
+    end
+  end
+end
