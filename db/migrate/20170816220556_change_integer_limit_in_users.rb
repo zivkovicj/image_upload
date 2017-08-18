eaf96aca695e6282d540a3009168a62229af0da7
Jeff Zivkovic <zivkovic.jeff@gmail.com>
2017-08-18 17:48:59 +0000

    Now unnecessary migration is deleted

--- db/migrate/20170816220556_change_integer_limit_in_users.rb
+++
@@ -1,9 +0,0 @@
-class ChangeIntegerLimitInUsers < ActiveRecord::Migration[5.0]
-  def up
-    change_column :users, :user_number, :integer, limit: 4
-  end
-  
-  def down
-    change_column :users, :user_number, :integer, limit: 4
-  end
-end
