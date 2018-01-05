4c024644cd07b6f97cfcbd88e06b0d27dfd2e376
Jeff Zivkovic <zivkovic.jeff@gmail.com>
2018-01-05 23:51:50 +0000

    Why don't I just follow best practice!

--- db/migrate/20180105234602_add_extent_to_goal.rb
+++ db/migrate/20180105234602_add_extent_to_goal.rb
@@ -1,7 +1,5 @@
 class AddExtentToGoal < ActiveRecord::Migration[5.0]
   def change
-    add_column    :goals, :extent, :string
-    add_reference   :goals, :user, foreign_key: true
     add_column    :goal_students, :term, :integer
     add_column    :checkpoints, :sequence, :integer
   end
