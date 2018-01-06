6b5a15f8ef704f1dfcd85cd699de554b1ced4a96
Jeff Zivkovic <zivkovic.jeff@gmail.com>
2018-01-06 16:52:46 +0000

    Ready to begin 2.47

--- db/migrate/20180105234602_add_extent_to_goal.rb
+++ db/migrate/20180105234602_add_extent_to_goal.rb
@@ -1,6 +1,10 @@
 class AddExtentToGoal < ActiveRecord::Migration[5.0]
+  
   def change
     add_column    :goal_students, :term, :integer
     add_column    :checkpoints, :sequence, :integer
+    
+    add_column    :goals, :extent, :string
+    add_reference :goals, :user, foreign_key: true
   end
 end
