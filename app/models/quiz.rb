83645fd9075da88997252f510d3f6895b80bc261
Jeff Zivkovic <zivkovic.jeff@gmail.com>
2018-08-25 15:44:14 +0000

    Fixed Quiz Score Display

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -8,12 +8,8 @@ class Quiz < ApplicationRecord
     belongs_to :user
     belongs_to :objective
     
-    def stars_from_score
-        (total_score/10.to_f).ceil
-    end
-    
     def added_stars
-        [self.stars_from_score - self.old_stars, 0].max
+        [self.total_score - self.old_stars, 0].max
     end
     
     private
